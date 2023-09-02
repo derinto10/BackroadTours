d92bf3cdb9444d1d2ac4eaa6933cab880c47c4df
derinto10 <derinajibade@gmail.com>
2023-09-02 08:27:26 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+
+//select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+//add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+//select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    //prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      //top: element.offsetTop,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
